218fec17578ce540763f64d94ca6997f78c7c618
aryan750 <aryangupta750@gmail.com>
2022-03-09 13:42:51 +0530

    changed in notificationRepo

--- src/main/java/com/example/meconnect/repository/NotificationRepo.java
+++ src/main/java/com/example/meconnect/repository/NotificationRepo.java
@@ -14,7 +14,7 @@ public interface NotificationRepo extends JpaRepository<Notification, Long> {
     Notification save(Notification notification);
 
     @Transactional
-    @Query(value = "SELECT n.* FROM notification n WHERE n.user_name != ?1 ORDER BY n.created_at DESC", nativeQuery = true)
+    @Query(value = "SELECT n.* FROM notification n WHERE n.user_name = ?1 AND n.created_by != ?1 ORDER BY n.created_at DESC", nativeQuery = true)
     List<Notification> findNotification(String userName);
 }
 //WHERE user_name = ?1 AND is_read = 0
